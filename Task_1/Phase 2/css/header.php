200042b63a6251981313055e21627cce06d496b5
Revanth7154 <xylomastero@gmail.com>
2016-05-30 18:27:33 +0530

    Created Folders for Each phase

---
+++ Task_1/Phase 2/css/header.php
@@ -0,0 +1,73 @@
+<div id="popular" class="popular">
+				<div class="clearfix">
+					<h3>Popular posts</h3>
+					<div class="gallery-nav">
+						<p class="prev"><i class="fa fa-angle-left"></i></p>
+						<p class="next"><i class="fa fa-angle-right"></i></p>
+					</div>
+				</div>
+				<div class="row gallery">
+																			<article class="col-md-3 col-sm-3">
+						<div class="img">
+							<img src="#">
+							<a href="#"><div class="overlay"></div></a>
+						</div>
+						<div class="info">
+							<p class="tags">
+																						<a href="#">Halls</a>
+														</p>
+							<h1><a href="#">Twin Trouble: The Tale of the Two Ladies’ Halls </a></h1>
+						</div>
+						<ul class="counters list-inline">
+							<li>
+								<a href="#"><i class="fa fa-eye"></i>11098</a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-comment"></i>2</a>
+							</li>
+						</ul>
+					</article>
+															<article class="col-md-3 col-sm-3">
+						<div class="img">
+							<img src="">
+							<a href=""><div class="overlay"></div></a>
+						</div>
+						<div class="info">
+							<p class="tags">
+																						<a href="">Campus Buzz</a>
+														</p>
+							<h1><a href="">The Rising Phoenix</a></h1>
+						</div>
+						<ul class="counters list-inline">
+							<li>
+								<a href="#"><i class="fa fa-eye"></i>10261</a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-comment"></i>1</a>
+							</li>
+						</ul>
+					</article>
+															<article class="col-md-3 col-sm-3">
+						<div class="img">
+							<img src="" alt="Placement Plans in the Pipeline">
+							<a href=""><div class="overlay"></div></a>
+						</div>
+						<div class="info">
+							<p class="tags">
+																						<a href="">Placements</a>
+														</p>
+							<h1><a href="">Placement Plans in the Pipeline</a></h1>
+						</div>
+						<ul class="counters list-inline">
+							<li>
+								<a href="#"><i class="fa fa-eye"></i>7155</a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-comment"></i>4</a>
+							</li>
+						</ul>
+					</article>
+												
+			</div>
+				<div class="pager"></div>
+			</div>
